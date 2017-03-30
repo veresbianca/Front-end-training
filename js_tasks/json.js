bc1e4387e3053176807770e74dbc541803ecfdaa
biancaveres <biancaveres93@gmail.com>
2017-03-30 12:59:20 +0300

    JS tasks

---
+++ js_tasks/json.js
@@ -0,0 +1,23 @@
+/**
+ * Created by biancaveres on 30/03/2017.
+ */
+var data={"users":[
+    {
+        "firstName":"Ray",
+        "lastName":"Villalobos",
+        "joined": {
+            "month":"January",
+            "day":12,
+            "year":2012
+        }
+    },
+    {
+        "firstName":"John",
+        "lastName":"Jones",
+        "joined": {
+            "month":"April",
+            "day":28,
+            "year":2010
+        }
+    }
+]}
